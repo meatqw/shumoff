c2293b191c080c77d895a2172c28477284a28c88
olega211 <lexerdxx@gmail.com>
2022-10-07 05:06:20 +0700

    new base models

--- application/models.py
+++ application/models.py
@@ -2,32 +2,73 @@ from application import db
 from datetime import datetime
 
 # DB Model PRODUCT
-class Product(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    vendor = db.Column(db.String(200), nullable=True)
-    title = db.Column(db.String(200), nullable=True)
-    desc = db.Column(db.TEXT, nullable=True)
-    attr = db.Column(db.TEXT, nullable=True)
-    price = db.Column(db.Float(), nullable=True)
-    size = db.Column(db.String(400), nullable=True)
-    type = db.Column(db.String(400), nullable=True)
-    link = db.Column(db.String(400), nullable=True)
-    datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
+# class Product(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     vendor = db.Column(db.String(200), nullable=True)
+#     title = db.Column(db.String(200), nullable=True)
+#     desc = db.Column(db.TEXT, nullable=True)
+#     attr = db.Column(db.TEXT, nullable=True)
+#     price = db.Column(db.Float(), nullable=True)
+#     size = db.Column(db.String(400), nullable=True)
+#     type = db.Column(db.String(400), nullable=True)
+#     link = db.Column(db.String(400), nullable=True)
+#     datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
 
 # DB Model BODY
-class Body(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(200), nullable=True)
-    effect_max = db.Column(db.JSON, nullable=True)
-    effect_great = db.Column(db.JSON, nullable=True)
-    effect_good = db.Column(db.JSON, nullable=True)
-    effect_practical = db.Column(db.JSON, nullable=True)
-    datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
+# class Body(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(200), nullable=True)
+#     effect_max = db.Column(db.JSON, nullable=True)
+#     effect_great = db.Column(db.JSON, nullable=True)
+#     effect_good = db.Column(db.JSON, nullable=True)
+#     effect_practical = db.Column(db.JSON, nullable=True)
+#     datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
     
-# DB Model Auto
-class Auto(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    brand = db.Column(db.String(200), nullable=True)
-    model = db.Column(db.String(200), nullable=True)
-    body = db.Column(db.Integer(), db.ForeignKey('body.id'), nullable=True)
-    datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
+# # DB Model Auto
+# class Auto(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     brand = db.Column(db.String(200), nullable=True)
+#     model = db.Column(db.String(200), nullable=True)
+#     body = db.Column(db.Integer(), db.ForeignKey('body.id'), nullable=True)
+#     datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    
+class Product(db.Model):
+    post_title = db.Column(db.String(200), nullable=True) # Название товара
+    post_name = db.Column(db.String(200), nullable=True) # Транслитированое название товара
+    ID = db.Column(db.Integer(), primary_key=True)
+    post_content = db.Column(db.TEXT, nullable=True) # Подробное описание
+    post_status = db.Column(db.String(200), nullable=True) # всегда publish (Опубликовано)
+    menu_order = db.Column(db.Integer(), nullable=True) # плевать что тут будет
+    post_date = db.Column(db.String(200), nullable=True) # (2022-10-05 2:08:29) 
+    post_parent = db.Column(db.String(200), nullable=True) # родитель (0)
+    comment_status = db.Column(db.String(200), nullable=True) # всегда closed
+    sku = db.Column(db.Integer(), nullable=True) # артикул товара
+    downloadable  = db.Column(db.String(200), nullable=True) # всегда no
+    virtual  = db.Column(db.String(200), nullable=True) # всегда no
+    stock_status  = db.Column(db.String(200), nullable=True) # всегда instock
+    backorders  = db.Column(db.String(200), nullable=True) # всегда no
+    manage_stock  = db.Column(db.String(200), nullable=True) # всегда no
+    regular_price = db.Column(db.Integer(), nullable=True) # цена товара
+    tax_status  = db.Column(db.String(200), nullable=True) # всегда taxable
+    featured  = db.Column(db.String(200), nullable=True) # всегда no
+
+    tax_product_type = db.Column(db.String(200), nullable=True) # всегда variable
+    tax_product_cat = db.Column(db.String(200), nullable=True) # Название категории товара
+    attribute_pa_size = db.Column(db.String(400), nullable=True) # список типов упаковки, разделитель | например ( Коробка|лист 750 х 1000 мм|Лист 750 х 460 мм|Лист 750 х 500 мм|лист 750 х 560 мм|м² )
+    attribute_data_pa_size = db.Column(db.String(200), nullable=True) # выбранные атрибуты, разделитель | например (0|1|1)
+
+    
+class Options(db.Model):
+    Parent  = db.Column(db.String(200), nullable=True) # Название родительского товара
+    parent_sku = db.Column(db.Integer(), nullable=True) # артикул родительского товара
+    post_parent = db.Column(db.Integer(), nullable=True) # ID родительского товара
+    ID = db.Column(db.Integer(), primary_key=True) # ID вариации
+    post_status  = db.Column(db.String(200), nullable=True) #- всегда publish (Опубликовано)
+    menu_order = db.Column(db.Integer(), nullable=True) # плевать что тут будет
+    sku = db.Column(db.Integer(), nullable=True) # артикул вариации (если отличается от родительской)
+    downloadable  = db.Column(db.String(200), nullable=True) # всегда no
+    virtual  = db.Column(db.String(200), nullable=True) # всегда no
+    stock_status  = db.Column(db.String(200), nullable=True) # всегда instock
+    regular_price = db.Column(db.Integer(), nullable=True) # цена  вариации (если отличается от родительской)
+    tax_class   = db.Column(db.String(200), nullable=True) # всегда parent
+    meta_attribute_pa_size  = db.Column(db.String(200), nullable=True) # Транслитированое значение из поля attribute:pa_size родительского товара, с заменой пробелов на - и приведением всех букв в ловеркейс, например (лист 750 х 1000 мм  =>   list-750-h-1000-mm)
--- catalog.py
+++ catalog.py
@@ -1,6 +1,9 @@
+from itertools import product
 from static import get_content
-from application.models import Product
+from application.models import Product, Options
 from application import db
+from transliterate import translit
+from datetime import datetime
 
 
 catalog_links = ['https://shumoff.biz/vibration_absorbing', 'https://shumoff.biz/soundproofing',
@@ -21,7 +24,9 @@ def get_data_all_elements(url):
         list = content.find(
             "section", {'class': 'shop-cat-content-list'}).find('ul').find_all('li')
         for item in list:
-
+                
+                
+            product_arr = []
             try:
                 vendor = item.get('class')[0].replace('sw', '')  # vendor
             except Exception as e:
@@ -54,7 +59,7 @@ def get_data_all_elements(url):
                 data = {'vendor': vendor, 'title': title, 'desc': desc, 'attr': attr,
                         'price': price, 'size': size, 'type': type_, 'link': link}
 
-                result_catalog_data.append(data)
+                product_arr.append(data)
 
             # more product options
             if variants != None:
@@ -66,7 +71,9 @@ def get_data_all_elements(url):
                     data = {'vendor': vendor, 'title': title, 'desc': desc, 'attr': attr,
                             'price': price, 'size': size, 'type': type_, 'link': link}
 
-                    result_catalog_data.append(data)
+                    product_arr.append(data)
+            
+            result_catalog_data.append(product_arr)
 
 
 def main():
@@ -75,20 +82,80 @@ def main():
         get_data_all_elements(link)
 
     for data in result_catalog_data:
-
+        
+        vendor = data[0]['vendor']
+        title = data[0]['title']
+        title_transliteration =  translit(data[0]['title'], language_code='ru', reversed=True)
+        desc = data[0]['desc']
+        attr = data[0]['attr']
+        price = data[0]['price']
+        size = data[0]['size']
+        type_ = data[0]['type']
+        link = data[0]['link']
+        date = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+        
+        all_size = ''
+        
+        for i in data:
+            if size != None:
+                all_size += i['size']+'|'
+                
+            
+        all_size = all_size[:-1]
+        
         # add data to base
         product = Product(
-            vendor=data['vendor'],
-            title=data['title'],
-            desc=data['desc'],
-            attr=data['attr'],
-            price=data['price'],
-            size=data['size'],
-            type=data['type'],
-            link=data['link'])
+            post_title = title,
+            post_name = title_transliteration,
+            # ID = 
+            post_content = desc,
+            post_status = 'publish',
+            menu_order = 0,
+            post_date = date,
+            post_parent = 0,
+            comment_status = 'closed',
+            sku = int(vendor),
+            downloadable  = 'no',
+            virtual  = 'no',
+            stock_status  = 'instock',
+            backorders  = 'no',
+            manage_stock  = 'no',
+            regular_price = price,
+            tax_status  = 'taxable',
+            featured  = 'no',
 
+            tax_product_type = 'variable',
+            tax_product_cat = type_,
+            attribute_pa_size = all_size,
+            attribute_data_pa_size = '')
+        
         db.session.add(product)
         db.session.commit()
         
+        product_id = product.ID
+        
+        
+        for i in data:
+            if size != None:
+                size = translit(i['size'].strip().replace(' ', '-'), language_code='ru', reversed=True)
+        
+                # add data to base
+                options = Options(
+                    Parent  = title,
+                    parent_sku = vendor,
+                    post_parent = product_id,
+                    post_status  = 'publish',
+                    menu_order = 0,
+                    downloadable  = 'no',
+                    virtual  = 'no',
+                    stock_status  = 'instock',
+                    regular_price = int(i['price']),
+                    tax_class   = 'parent',
+                    meta_attribute_pa_size  = size)
+
+                db.session.add(options)
+                db.session.commit()
+                
+        print(all_size, date, title_transliteration)
 main()
 
